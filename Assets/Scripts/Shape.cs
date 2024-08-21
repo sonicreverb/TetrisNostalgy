f8cbbba9fa9d137d9af467f205c0b3ac6d252005
sonicreverb <110459178+sonicreverb@users.noreply.github.com>
2024-08-21 22:32:33 +0300

    Basic rotation + movement added
    (need fixes)

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
+    const float _blockLength = 0.35f;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        moveCorutine = StartCoroutine(MoveDown());
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        handleInput();
+    }
+
+
+    // Shape falling 
+    Coroutine moveCorutine = null;
+
+    IEnumerator MoveDown() {
+       while (true) {
+            transform.Translate(0, -_blockLength, 0, Space.World);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    void handleInput() {
+        // Movement
+        float xOffset = 0;
+        float yOffset = 0;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            xOffset = -_blockLength;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            xOffset = _blockLength;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) { 
+            yOffset = -_blockLength;
+        }
+
+        transform.Translate(xOffset, yOffset, 0, Space.World);
+
+        // Rotation
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            transform.eulerAngles += new Vector3(0, 0, -90);
+        }
     }
 }
